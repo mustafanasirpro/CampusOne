5ea39f954f781fb2a63f6f0948f98859068d263a
Mustafa Nasir <mustafanasirpro@gmail.com>
2026-07-05 23:23:31 +0500

    feat(storage): update InvalidFileUploadException.java

---
+++ backend/src/main/java/com/campusone/common/exception/InvalidFileUploadException.java
@@ -0,0 +1,8 @@
+package com.campusone.common.exception;
+
+public class InvalidFileUploadException extends RuntimeException {
+
+    public InvalidFileUploadException(String message) {
+        super(message);
+    }
+}
