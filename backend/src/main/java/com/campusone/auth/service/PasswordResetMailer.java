5424ebc6754ab3bc7a0ea6cb7cc8e3d79f00fe0e
Mustafa Nasir <mustafanasirpro@gmail.com>
2026-07-09 04:35:42 +0500

    fix(auth): update PasswordResetMailer.java

--- backend/src/main/java/com/campusone/auth/service/PasswordResetMailer.java
+++ backend/src/main/java/com/campusone/auth/service/PasswordResetMailer.java
@@ -5,4 +5,6 @@ import com.campusone.user.entity.User;
 public interface PasswordResetMailer {
 
     void sendResetLink(User user, String rawToken);
+
+    void sendTestEmail(String recipientEmail);
 }
